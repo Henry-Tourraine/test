cef8757fee83afa7e1a1bcab5ef0baaa6dccad22
Henry-Tourraine <henry.tourraine@outlook.fr>
2020-12-30 06:29:53 +0100

    text

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const express = require('express')
 app = express();
 
 
+app.get('/', (req, res)=>{
+    res.send("CONNECTED")
+})
+
 port = process.env.PORT || 3000;
 app.listen(port, ()=>{console.log("Connecté !")})
